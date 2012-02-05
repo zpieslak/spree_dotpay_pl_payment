5993afb55aeb34e2599c71aa33092651652ce077
Mariusz Drozdziel <mdrozdziel@gmail.com>
2012-02-06 00:57:20 +0100

    add urlc

--- app/models/payment_method/dotpay_pl.rb
+++ app/models/payment_method/dotpay_pl.rb
@@ -3,7 +3,8 @@ class PaymentMethod::DotpayPl < PaymentMethod
   preference :account_id, :string
   preference :pin, :string
   preference :url, :string, :default => "https://ssl.dotpay.pl/"
-  preference :type, :string, :default => "3"
+  preference :urlc, :string, :default => "https://example.com/gateway/dotpay_pl/comeback/"
+  preference :type, :string, :default => "0"
   preference :currency, :string, :default => "PLN"
   preference :language, :string, :default => "pl"
   preference :dotpay_server_1, :string, :default => "217.17.41.5"
